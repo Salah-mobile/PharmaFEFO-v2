98269421514746abdd59a063c3b441bd56b42e52
Salah-mobile <tabitsalah12@gmail.com>
2026-06-15 17:44:52 +0100

    create the structure of the project

---
+++ config/connection.php
@@ -0,0 +1,20 @@
+<?php
+class Database
+{
+    private static ?PDO $instance = null;
+    public static function getConnection(): PDO
+    {
+        if (self::$instance === null) {
+            try {
+                self::$instance = new PDO(
+                    'mysql:host=localhost;dbname=pharmafefo_db;charset=utf8',
+                    'root',
+                    '',
+                );
+            } catch (PDOException $exception) {
+                die('Erreur de connexion base de données : ' . $exception->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+$action = $_GET["action"] ?? "login";
+switch ($action) {
+    case 'login':
+        
+        break;
+    
+    default:
+        # code...
+        break;
+}
