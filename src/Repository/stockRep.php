743dedd904085590d762ec0a82e4ffe8e7e98e08
Salah-mobile <tabitsalah12@gmail.com>
2026-06-18 17:41:25 +0100

    create the functions of the stock repo

---
+++ src/Repository/stockRep.php
@@ -0,0 +1,43 @@
+<?php
+namespace Repository;
+use config\connection;
+class stockRep{
+    private $connection;
+    public function __construct(){
+        $this->$connection=connection::getConnection();
+    }
+    public function getAllProduct(){
+        try {
+            $sql="SELECT l.id,p.id,p.nom,p.reference,p.prix,l.quantite,l.date_peremption,l.numero_lot,l.statut 
+            FROM produits p 
+            JOIN lot_stocks l ON P.id=l.produit_id";
+            $stm=$this->connection->prepare($sql);
+            $stm->execute();
+            $res=$stm->fetchAll();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function addNewLot($product_id,$numLot,$quantite,$datePer,$status){
+        try {
+            $sql="INSERT INTO lot_stocks (produit_id,numero_lot,quantite,date_peremption,statut) 
+            values($product_id,$numLot,$quantite,$datePer,$status)";
+            $stm=$this->connection->prepare($sql);
+            $stm->execute($status);
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function updateStatusLots($lot_id,$newSt){
+          try {
+            $sql="UPDATE lot_stocks set statut=? WHERE lot_stocks.id=?";
+            $stm=$this->connection->prepare($sql);
+            $stm->execute([$newSt,$lot_id]);
+        } catch (PDOException $e) {
+            return $e->getMessage();
+          }
+    }
+   
+    
+}
